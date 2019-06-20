dbb89a4a1a1b6c42f42b81668a8f3a5a5636798e
yakiniku-ya <rikuto.tsujimoto@gmail.com>
2019-06-21 01:40:35 +0900

    キーワード検索機能の実装

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,6 +2,11 @@ const url = "https://api.gnavi.co.jp/RestSearchAPI/v3/"
 // const key = "6526d105b1d4506fdb09b7d64afa244b"
 const key = "578f48400166717797a5f1da5f50e583"
 
+/*
+    直近課題：
+        input-textでエンター押したら強制的にsubmitされてしまう（更新？）のをなんとかする
+*/
+
 const main = new Vue({
     el: '#main',
     data: {
@@ -9,8 +14,10 @@ const main = new Vue({
         longitude: '',
         page: 0,
         pages: 1,
+        freeword: '',
         selected: '2',
         options: [
+            {text: '指定しない', value: ''},
             {text: '300m', value: '1'},
             {text: '500m', value: '2'},
             {text: '1km', value: '3'},
@@ -22,24 +29,90 @@ const main = new Vue({
         results: []
     },
     mounted () {
-        this.restSearch();
+        this.searchHandler();
     },
     methods: {
-        restSearch: function ($state) {
-            // if-elseで再帰的にやってるけど非同期処理？したほうがいいかも
+        searchHandler: function () {
             if (this.latitude == '' || this.longitude == '') {
                 this.getLocation();
             } else {
-                axios
-                .get(url, {
-                    params: {
-                        keyid: key,
-                        range: this.selected,
-                        latitude: this.latitude,
-                        longitude: this.longitude,
-                        offset_page: this.page + 1,
-                        hit_per_page: 30
+                if (this.selected == '') {
+                    this.restSearch(
+                        {
+                            keyid: key,
+                            freeword: this.freeword,
+                            offset_page: this.page + 1,
+                            hit_per_page: 30
+                        }
+                    );
+                } else {
+                    this.restSearch(
+                        {
+                            keyid: key,
+                            range: this.selected,
+                            latitude: this.latitude,
+                            longitude: this.longitude,
+                            freeword: this.freeword,
+                            offset_page: this.page + 1,
+                            hit_per_page: 30
+                        }
+                    );
+                }
+            }
+        },
+
+        getLocation: function () {
+            navigator.geolocation.getCurrentPosition (
+                function(position) {
+                    main.latitude = position.coords.latitude;
+                    main.longitude = position.coords.longitude;
+                    main.searchHandler();
+                },
+                function(error) {
+                    var message = '';
+                    switch (error.code) {
+                        case 1: 
+                            message = 'ブラウザの位置情報の利用を許可してください。'; break;
+                        case 2: 
+                            message = '端末の位置を判定できません。'; break;
+                        case 3:
+                            message = 'リクエストがタイムアウトしました。'; break;
                     }
+                    main.error_message = message;
+                }
+            )
+        },
+
+        submit: function () {
+            // Searchボタンをおしてsubmitした時の挙動というかpageとかのリセット。
+            this.page = 0;
+            this.pages = -1;
+            this.results = [];
+            this.searchHandler();
+        },
+
+        prevPage: function () {
+            if (0 >= this.page) {
+                return;
+            }
+            this.page -= 1;
+            this.results = [];
+            this.searchHandler();
+        },
+
+        nextPage: function () {
+            if (this.pages <= this.page + 1) {
+                return;
+            }
+            this.page += 1;
+            this.results = [];
+            this.searchHandler();
+        },
+        
+        restSearch: function (params)  {
+            axios
+                .get(url, {
+                    params: params
                 })
                 .then(response => {
                     this.error_message = '';
@@ -78,55 +151,7 @@ const main = new Vue({
                     }
                     this.error_message = message;
                 })
-            }
+            
         },
-
-        getLocation: function () {
-            navigator.geolocation.getCurrentPosition (
-                function(position) {
-                    main.latitude = position.coords.latitude;
-                    main.longitude = position.coords.longitude;
-                    main.restSearch();
-                },
-                function(error) {
-                    var message = '';
-                    switch (error.code) {
-                        case 1: 
-                            message = 'ブラウザの位置情報の利用を許可してください。'; break;
-                        case 2: 
-                            message = '端末の位置を判定できません。'; break;
-                        case 3:
-                            message = 'リクエストがタイムアウトしました。'; break;
-                    }
-                    main.error_message = message;
-                }
-            )
-        },
-
-        submit: function () {
-            // Searchボタンをおしてsubmitした時の挙動というかpageとかのリセット。
-            this.page = 0;
-            this.pages = -1;
-            this.results = [];
-            this.restSearch();
-        },
-
-        prevPage: function () {
-            if (0 >= this.page) {
-                return;
-            }
-            this.page -= 1;
-            this.results = [];
-            this.restSearch();
-        },
-
-        nextPage: function () {
-            if (this.pages <= this.page + 1) {
-                return;
-            }
-            this.page += 1;
-            this.results = [];
-            this.restSearch();
-        }
     }
 })
